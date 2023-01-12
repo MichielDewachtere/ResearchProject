10b7761a9bba3a9a1df3fdec9942de61a287aa2d
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-01-12 14:28:49 +0100

    Create MathUtils.h

---
+++ ResearchProject/MathUtils.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include "structs.h"
+
+inline auto DistanceSquared(const Point2f& v1, const Point2f& v2)
+{
+	return (v1.x - v2.x) * (v1.x - v2.x) + (v1.y - v2.y) * (v1.y - v2.y);
+}
